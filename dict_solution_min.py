522e34c4eff2e7f4fa637cfdf9d9b41921c71a14
Nathakan Techadis <nathpotter@gmail.com>
2023-06-20 20:36:07 -0600

    Create dict_solution_min.py
    min dict solution

---
+++ dict_solution_min.py
@@ -0,0 +1,26 @@
+#/usr/bin/python
+
+subjects = []
+grades = []
+
+mode = input('Enter mode (q - quit, a = add): ')
+
+while mode != 'q':
+    if mode == 'a':
+        subject = input('Enter subject: ')
+        grade = int(input('Enter grade: '))
+        
+        subjects.append(subject)
+        grades.append(grade)
+
+    for subject in subjects:
+        print(subject)
+        
+    for grade in grades:
+        print(grade)
+
+    mode = input('Enter mode (q - quit, a = add): ')
+    if mode == 'q':
+        res_dict = dict(zip(subjects, grades))
+        print(res_dict)
+        print('Subject that has minimum score: ', min(res_dict, key=res_dict.get))
